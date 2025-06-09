731b19485c16da599c5687125fd74bea709c5dda
GenmaHimmuro <t.suleimanov2@gmail.com>
2025-06-09 15:34:16 +0500

    setup Flat changelist page

--- property/models.py
+++ property/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils import timezone
-from django.contrib.auth.models import User
-from phonenumber_field.modelfields import PhoneNumberField
+# from django.contrib.auth.models import User
+# from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Flat(models.Model):
